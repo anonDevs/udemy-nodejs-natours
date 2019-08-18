181947f61eac231c5ad2d89184e22494ec5dc1f3
AnonDevs <hussain.abeer2593@gmail.com>
2019-08-18 23:15:50 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+
+//IMPORT ROUTERS
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+//DECLARE THE APP
+const app = express();
+
+//MIDDELWARE
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  //console.log('Hello from the middleware 😄');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.requestTime);
+  next();
+});
+
+//MOUNTING THE ROUTERS
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
